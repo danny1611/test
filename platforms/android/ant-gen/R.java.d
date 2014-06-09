24636431404beabd50c2f54d74124467cfce5397
Daniel Adrian Gargiulo <dannyelpetizo@hotmail.com>
2014-06-09 00:30:07 -0300

    Initial Commit RD Ltam BR

--- platforms/android/ant-gen/R.java.d
+++ platforms/android/ant-gen/R.java.d
@@ -14,17 +14,4 @@ C:\AppServ\www\CordovaTest\platforms\android\res\drawable-port-xhdpi\screen.png 
 C:\AppServ\www\CordovaTest\platforms\android\res\drawable-xhdpi\icon.png \
 C:\AppServ\www\CordovaTest\platforms\android\res\values\strings.xml \
 C:\AppServ\www\CordovaTest\platforms\android\res\xml\config.xml \
-C:\AppServ\www\CordovaTest\platforms\android\ant-build\res\drawable\icon.png \
-C:\AppServ\www\CordovaTest\platforms\android\ant-build\res\drawable-hdpi\icon.png \
-C:\AppServ\www\CordovaTest\platforms\android\ant-build\res\drawable-land-hdpi\screen.png \
-C:\AppServ\www\CordovaTest\platforms\android\ant-build\res\drawable-land-ldpi\screen.png \
-C:\AppServ\www\CordovaTest\platforms\android\ant-build\res\drawable-land-mdpi\screen.png \
-C:\AppServ\www\CordovaTest\platforms\android\ant-build\res\drawable-land-xhdpi\screen.png \
-C:\AppServ\www\CordovaTest\platforms\android\ant-build\res\drawable-ldpi\icon.png \
-C:\AppServ\www\CordovaTest\platforms\android\ant-build\res\drawable-mdpi\icon.png \
-C:\AppServ\www\CordovaTest\platforms\android\ant-build\res\drawable-port-hdpi\screen.png \
-C:\AppServ\www\CordovaTest\platforms\android\ant-build\res\drawable-port-ldpi\screen.png \
-C:\AppServ\www\CordovaTest\platforms\android\ant-build\res\drawable-port-mdpi\screen.png \
-C:\AppServ\www\CordovaTest\platforms\android\ant-build\res\drawable-port-xhdpi\screen.png \
-C:\AppServ\www\CordovaTest\platforms\android\ant-build\res\drawable-xhdpi\icon.png \
 C:\AppServ\www\CordovaTest\platforms\android\ant-build\AndroidManifest.xml \
